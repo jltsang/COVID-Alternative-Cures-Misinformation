d42a97626c172bd2140f033ed517d6d616de4a8a
rudnam <francisalbarracin2@gmail.com>
2023-06-10 13:58:58 +0800

    Delegate formatting fixes to js script

---
+++ assets/js/formatting.js
@@ -0,0 +1,24 @@
+// Get all code blocks in the HTML
+const codeBlocks = document.querySelectorAll('code');
+
+// Iterate through each code block
+codeBlocks.forEach((block) => {
+  // Get the content of the code block
+  let content = block.innerHTML;
+
+	// Remove leading and trailing whitespace
+  content = content.trim();
+
+  // Find the minimum number of tabs in a line
+  const tabsMatch = content.match(/^\t+/gm);
+  const tabsCount = tabsMatch ? tabsMatch.reduce((min, tabs) => Math.min(min, tabs.length), Infinity) : 0;
+  console.log(tabsCount)
+
+  // Remove tabs from each line until one line has no more tabs
+  content = content.replace(new RegExp(`^\t{${tabsCount}}`, 'gm'), '');
+
+  // Update the content of the code block
+  block.innerHTML = content;
+});
+
+
